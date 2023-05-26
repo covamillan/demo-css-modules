732ce9c3fcf3580a0d67641b317262ef6015da2f
CovaM <cova.millan@gmail.com>
2023-05-26 12:33:49 +0200

    Add more little changes

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.scss";
 import { App } from "./components/app/app";
+import "./index.scss";
 
 ReactDOM.createRoot(document.querySelector("#root") as HTMLElement).render(
   <React.StrictMode>
