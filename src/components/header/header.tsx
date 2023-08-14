eddb592b8b4caf8a994b61d24f104f04d5ec7eed
CovaM <cova.millan@gmail.com>
2023-08-14 16:44:47 +0200

    Update examples

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,10 +3,13 @@ import styles from "./header.module.scss";
 export function Header() {
   return (
     <>
-      <header className={styles.header}>
-        <h1 className={styles.headerH1}>
+      {/* Con corchetes si no queremos usar camel case */}
+      <header className={styles["main-header"]}>
+        {/*Con template strings si queréis poner dos clases  */}
+        <h1 className={`${styles.headerH1} ${styles.headerH1}`}>
           Hola soy un header moderno que usa CSS modules
         </h1>
+        {/* Con camel case si no queréis usar corchetes */}
         <h2 className={styles.headerH2}>
           Yo soy el h2 del header y estoy aquí para que veáis que no me afecta
           lo que se le diga al otro h2 del footer en el index.scss, porque como
